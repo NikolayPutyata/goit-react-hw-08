23ecb738c0da4952a2e158f29b63f257586c9ba1
NikolayPutyata <nikolayputyata@gmail.com>
2024-10-18 15:41:23 +0300

    upd

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import Navigation from "../Navigation/Navigation";
-import UserMenu from "../UserMenu/userMenu";
+import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/authNav";
 
 const AppBar = () => {
